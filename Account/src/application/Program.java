e0c20032b541fce2020c226a4f887ff4aa68ce63
Developer Maiara C Freitas <88987257+DevMaiaraFreitas@users.noreply.github.com>
2023-05-11 20:08:15 -0300

    created entities

---
+++ Account/src/application/Program.java
@@ -0,0 +1,16 @@
+
+package application;
+
+/**
+ *
+ * Developed by Maiara Cristina de Freitas
+ * 
+ */
+public class Program {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
