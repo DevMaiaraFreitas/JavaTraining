99b2b7ee30278cde673f67e7dfa16838b883fb50
Developer Maiara C Freitas <88987257+DevMaiaraFreitas@users.noreply.github.com>
2023-05-11 21:18:53 -0300

    Class bisinessAccount implemented

--- Account/src/entities/businessAccount.java
+++ Account/src/entities/businessAccount.java
@@ -6,6 +6,30 @@ package entities;
  * Developed by Maiara Cristina de Freitas
  * 
  */
-public class businessAccount {
+public class businessAccount extends Account{
     
+    private Double loanLimit;
+
+    public businessAccount() {
+        super();
+    }
+
+    public businessAccount(Integer number, String holder, Double balance, Double loanLimit) {
+        super(number, holder, balance);
+        this.loanLimit = loanLimit;
+    }
+
+    public Double getLoanLimit() {
+        return loanLimit;
+    }
+
+    public void setLoanLimit(Double loanLimit) {
+        this.loanLimit = loanLimit;
+    }
+    
+    public void loan(double amount){
+        if(amount <= loanLimit){
+            deposit(amount);
+        }
+    }
 }
