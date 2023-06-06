c884edf968d41e7669a74006b29d2e1eff264bf2
Developer Maiara C Freitas <88987257+DevMaiaraFreitas@users.noreply.github.com>
2023-06-06 20:41:34 -0300

    Overide implemented

--- outsourcedEmployee/outsourcedEmployer/src/entities/OutsourcedEmployee.java
+++ outsourcedEmployee/outsourcedEmployer/src/entities/OutsourcedEmployee.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package entities;
 
 /**
  *
- * @author camil
+ * @author Maiara Cristina Freitas
  */
-public class OutsourcedEmployee {
+public class OutsourcedEmployee extends Employee{
     
+   private Double additionalCharge;
+
+    public OutsourcedEmployee() {
+        super();
+    }
+
+    public OutsourcedEmployee(Double additionalCharge, String name, Integer hours, Double valuePerHour) {
+        super(name, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+    
+    @Override
+     public double payment(){
+        return super.payment()+additionalCharge*1.1;
+    }
 }
