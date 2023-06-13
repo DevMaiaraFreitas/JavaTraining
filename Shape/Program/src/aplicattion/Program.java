ffbf8391167666cb4545f71846e946240722d015
Developer Maiara C Freitas <88987257+DevMaiaraFreitas@users.noreply.github.com>
2023-06-13 19:38:19 -0300

    Project created

---
+++ Shape/Program/src/aplicattion/Program.java
@@ -0,0 +1,15 @@
+
+package aplicattion;
+
+/**
+ *
+ * @developed Maiara Cristina de Freitas
+ */
+public class Program {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
