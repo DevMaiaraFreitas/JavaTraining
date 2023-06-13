ea6bff52921dc5ff8d610df2a54d443184b08bc1
Developer Maiara C Freitas <88987257+DevMaiaraFreitas@users.noreply.github.com>
2023-06-13 19:55:55 -0300

    Class Shape and enums created

--- Shape/Program/src/aplicattion/Program.java
+++ Shape/Program/src/aplicattion/Program.java
@@ -9,7 +9,7 @@ public class Program {
 
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
---
+++ Shape/Program/src/entities/enums/Color.java
@@ -0,0 +1,12 @@
+
+package entities.enums;
+
+/**
+ *
+ * @developed Maiara Cristina de Freitas
+ */
+public enum Color {
+    BLACK,
+    BLUE,
+    RED;
+}
